e7e24aecea6587705290adc899444f8f6f21b0ee
shajith26 <mohamedshajith321@gmail.com>
2022-11-09 08:56:32 +0530

    log-in

---
+++ react-login/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Pages/Login/login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      {/* login */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-login/src/Pages/Login/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+        <div>Login</div>
+        <div><input placeholder={"ID"}></input></div>
+        <div><input placeholder={"password"}></input></div>
+        <div><button>Submit</button></div>
+    </div>
+  )
+}
+
+export default login
