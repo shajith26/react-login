ac08a363f3880cf154b5ce0184d0d1666df93a85
shajith26 <mohamedshajith321@gmail.com>
2022-11-15 12:59:07 +0530

    log-in

--- react-login/src/Pages/Login/login.js
+++ react-login/src/Pages/Login/login.js
@@ -1,14 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const login = () => {
+
+
+const Login = () => {
+
+  const [username, setusername]= useState("");
+  const [userpwd, setpassword]=useState("");
+  const [error, seterror]=useState('');
+
+  // const handleUserName = (ev) => {
+  //   console.log("ev",ev);
+  //   setusername(ev.target.value);
+  // };
+
+  // const handleUserPwd= (eve) => {
+  //   console.log("eve",eve);
+  //   setpassword(eve.target.value);
+  // };
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    if(username==="" || userpwd===""){
+      seterror("invalid user id or password");
+      return;
+    }
+      seterror(""); 
+    
+     
+    console.log("state",username,userpwd);
+  }
+
+  const handleInputChange = (ev) => {
+    console.log("ev",ev)
+    if(ev.target.value === ""){
+      seterror("invalid")
+    }else{
+      seterror("");
+    }
+    if(ev.target.value === "username"){
+      setusername(ev.target.value)
+    }else{
+      setpassword(ev.target.value)
+    }
+  }
+
   return (
     <div>
         <div>Login</div>
-        <div><input placeholder={"ID"}></input></div>
-        <div><input placeholder={"password"}></input></div>
-        <div><button>Submit</button></div>
+        <form onSubmit={handleSubmit}>
+        <div><input placeholder={"ID"} name="name" onChange={handleInputChange} /></div>
+        <div><input placeholder={"password"} type="password" name="password" onChange={handleInputChange} /></div>
+        <input type="submit"/>
+        <h3>{error}</h3>
+        {/* <div><button onClick={()=> handleSubmit()}>Submit</button></div> */}
+        </form>
     </div>
   )
 }
 
-export default login
+export default Login
